2d109eed799155a882d92cfd631675d5d820608d
DeFalT / GR6Y <47110374+DeFalT-910@users.noreply.github.com>
2022-06-20 11:37:58 -0400

    Backup.sh
    + New start

--- backup.sh
+++ backup.sh
@@ -1,17 +1,23 @@
-#!/bin/bash
-# script by GR6Y
-# version 0.0.1
+#!/usr/bin/env sh
+# Script by GR6Y
 
-#Arquivos que iram para backup
-backup_arquivos="/root/server/worlds"
-#Destino dos arquvios
-backup_destino="/root/backup/arquivos/"
+# Arquivos para backup
+backup_path="/root/server/worlds"
 
-# Var. de data
-data=$(date +%d/%m/%y-%T)
+# Destino do backup
+backup_dir="/root/backup/arquivos"
 
-#Arquivo final
-arquivo_final="Backup-[$data].tar.gz"
+# Formato do arquivo
+formato_data=$(date "+%d-%m-%y")
 
-#Compactador do arquivo
-tar -zcvf $backup_destino/$arquivo_final $backup_arquivos
+# Backup
+arquivo_final="Backup-$formato_data.tar.gz"
+
+# log Backup
+log_backup="/root/backup/mserver-backup.log"
+    $log_backup
+
+#############################
+# inicio do backup
+#############################
+tar -czSpf "$backup_dir/$Arquivo_final" "$backup_path"
